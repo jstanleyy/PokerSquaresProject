299e39bb9d61dac259aa9c12be0488e368bd2a95
Joel Stanley <stanleyj@live.ca>
2016-03-30 12:03:30 -0300

    Added comments.

--- Poker Squares/src/JstanleyPokerSquaresPlayer.java
+++ Poker Squares/src/JstanleyPokerSquaresPlayer.java
@@ -9,26 +9,10 @@ public class JstanleyPokerSquaresPlayer implements PokerSquaresPlayer {
 
 	private final int SIZE = 5; // number of rows/columns in square grid
 	private final int NUM_POS = SIZE * SIZE; // number of positions in square grid
-	private final int NUM_CARDS = Card.NUM_CARDS; // number of cards in deck
 	private PokerSquaresPointSystem system; // point system
 	private Card[][] grid = new Card[SIZE][SIZE]; // grid with Card objects or null (for empty positions)
-	private int numPlays = 0; // number of Cards played into the grid so far
-	private int[] plays = new int[NUM_POS];
 	private Random random = new Random(); // A random number generator for breaking minimax score ties
 	private final int OTHER_COL = 4;
-	
-	/**
-	 * Creates a Jstanley player with the default depth limit of 2.
-	 */
-	public JstanleyPokerSquaresPlayer() {
-	}
-	
-	/**
-	 * Creates a Jstanley player with a specified depth limit.
-	 * @param depthIn The desired depth limit of the player.
-	 */
-	public JstanleyPokerSquaresPlayer(int depthIn) {
-	}
 	
 	/* (non-Javadoc)
 	 * @see PokerSquaresPlayer#setPointSystem(PokerSquaresPointSystem, long)
@@ -49,13 +33,6 @@ public class JstanleyPokerSquaresPlayer implements PokerSquaresPlayer {
 				grid[row][col] = null;
 			}
 		}
-		
-		numPlays = 0; // reset numPlays
-		
-		// (re)initialize list of play positions (row-major ordering)
-		for(int i = 0; i < NUM_POS; i++) {
-			plays[i] = i;
-		}
 	}
 
 	/* (non-Javadoc)
@@ -63,11 +40,14 @@ public class JstanleyPokerSquaresPlayer implements PokerSquaresPlayer {
 	 */
 	@Override
 	public int[] getPlay(Card card, long millisRemaining) {
-		int[] play;
-		int suit = card.getSuit();
+		int[] play; // The play to be returned
+		int suit = card.getSuit(); // Used for placing the card in a specific column
 		
+		// If the column for that suit is full, place in the extra cards column
 		if(isColFull(suit)) {
+			// If the extra cards column is full, place in the first open column
 			if(isColFull(OTHER_COL)) {
+				// Loops to find the first column with a free space
 				boolean done = false;
 				int col = 0;
 				while(!done) {
@@ -78,26 +58,31 @@ public class JstanleyPokerSquaresPlayer implements PokerSquaresPlayer {
 						done = true;
 					}
 				}
-				play = makePlay(card, col);
+				play = makePlay(card, col); // Places the card in that column
 			}
 			else {
-				play = makePlay(card, OTHER_COL);
+				play = makePlay(card, OTHER_COL); // Places the card in the first open slot in the extra column
 			}
 		}
 		else {
-			play = makePlay(card, suit);
+			play = makePlay(card, suit); // Places the card in the first open slot in that suits column
 		}
 		
-		return play;
+		return play; // Returns the chosen play
 	}
 
+	/**
+	 * This method finds the first open space in the given column, and updates the grid to reflect that move.
+	 * @param card The card to be place.
+	 * @param col The column that the card will be placed in.
+	 * @return An array containing the chosen row and column.
+	 */
 	private int[] makePlay(Card card, int col) {
 		boolean done = false;
 		int row = 0;
 		while(!done) {
 			if(grid[row][col] == null) {
 				grid[row][col] = card;
-				numPlays++;
 				done = true;
 			}
 			else {
@@ -108,6 +93,11 @@ public class JstanleyPokerSquaresPlayer implements PokerSquaresPlayer {
 		return play;
 	}
 	
+	/**
+	 * Checks if the given column has a free space in it.
+	 * @param col The column to be checked.
+	 * @return Whether or not the given column has free space.
+	 */
 	private boolean isColFull(int col) {
 		boolean res = true;
 		for(int i = 0; i < grid.length; i++) {
@@ -137,7 +127,7 @@ public class JstanleyPokerSquaresPlayer implements PokerSquaresPlayer {
 		
 		System.out.println("\n\nBatch game demo:");
 		System.out.println(system);
-		new PokerSquares(new JstanleyPokerSquaresPlayer(), system).playSequence(10, 10000, false);
+		new PokerSquares(new JstanleyPokerSquaresPlayer(), system).playSequence(10, 0, false);
 	}
 
 }
